6770f5e4af226a62790a3d9a7690cbbbfcca8c31
chencheng8888 <2963801553@qq.com>
2025-09-03 16:58:56 +0800

    feat: 添加图书馆的相关tools

---
+++ internal/reverser/reverser_test.go
@@ -0,0 +1,74 @@
+package reverser
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestSeat_IsFreeByTime(t *testing.T) {
+	// 定义测试用例
+	tests := []struct {
+		name            string
+		seat            Seat
+		startTime       time.Time
+		endTime         time.Time
+		expectedFree    bool
+		expectedPeriods []Period
+	}{
+		{
+			name: "完全空闲",
+			seat: Seat{
+				ReserveStartTime: time.Date(2023, 10, 1, 8, 0, 0, 0, time.UTC),
+				ReserveEndTime:   time.Date(2023, 10, 1, 18, 0, 0, 0, time.UTC),
+				IsFree:           true,
+			},
+			startTime:       time.Date(2023, 10, 1, 9, 0, 0, 0, time.UTC),
+			endTime:         time.Date(2023, 10, 1, 10, 0, 0, 0, time.UTC),
+			expectedFree:    true,
+			expectedPeriods: []Period{{StartTime: time.Date(2023, 10, 1, 9, 0, 0, 0, time.UTC), EndTime: time.Date(2023, 10, 1, 10, 0, 0, 0, time.UTC)}},
+		},
+		{
+			name: "部分空闲",
+			seat: Seat{
+				ReserveStartTime: time.Date(2023, 10, 1, 8, 0, 0, 0, time.UTC),
+				ReserveEndTime:   time.Date(2023, 10, 1, 18, 0, 0, 0, time.UTC),
+				IsFree:           false,
+				OccupyStates: []Period{
+					{StartTime: time.Date(2023, 10, 1, 9, 0, 0, 0, time.UTC), EndTime: time.Date(2023, 10, 1, 10, 0, 0, 0, time.UTC)},
+				},
+			},
+			startTime:       time.Date(2023, 10, 1, 8, 30, 0, 0, time.UTC),
+			endTime:         time.Date(2023, 10, 1, 9, 30, 0, 0, time.UTC),
+			expectedFree:    false,
+			expectedPeriods: []Period{{StartTime: time.Date(2023, 10, 1, 8, 30, 0, 0, time.UTC), EndTime: time.Date(2023, 10, 1, 9, 0, 0, 0, time.UTC)}},
+		},
+		{
+			name: "完全被占用",
+			seat: Seat{
+				ReserveStartTime: time.Date(2023, 10, 1, 8, 0, 0, 0, time.UTC),
+				ReserveEndTime:   time.Date(2023, 10, 1, 18, 0, 0, 0, time.UTC),
+				IsFree:           false,
+				OccupyStates: []Period{
+					{StartTime: time.Date(2023, 10, 1, 8, 30, 0, 0, time.UTC), EndTime: time.Date(2023, 10, 1, 9, 30, 0, 0, time.UTC)},
+				},
+			},
+			startTime:       time.Date(2023, 10, 1, 8, 30, 0, 0, time.UTC),
+			endTime:         time.Date(2023, 10, 1, 9, 30, 0, 0, time.UTC),
+			expectedFree:    false,
+			expectedPeriods: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			free, periods := tt.seat.IsFreeByTime(tt.startTime, tt.endTime)
+			if free != tt.expectedFree {
+				t.Errorf("expected free: %v, got: %v", tt.expectedFree, free)
+			}
+			if !reflect.DeepEqual(periods, tt.expectedPeriods) {
+				t.Errorf("expected periods: %v, got: %v", tt.expectedPeriods, periods)
+			}
+		})
+	}
+}
